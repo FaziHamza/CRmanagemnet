4a1823cc16e5b0ffd64e3ccb148227771b50313c
arfanali278 <arfan7452@gmail.com>
2023-05-19 21:35:31 +0500

    order detail

--- src/app/main/pages/workorders/orderdetail/orderdetail.component.ts
+++ src/app/main/pages/workorders/orderdetail/orderdetail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/shared/services/api.service';
 
 @Component({
   selector: 'app-orderdetail',
@@ -9,53 +10,76 @@ import { ActivatedRoute } from '@angular/router';
 export class OrderdetailComponent implements OnInit {
   paramId:any;
   sparePartList: any[] = [];
-  pageSize = 6
-  constructor(private activeRoute:ActivatedRoute) { }
+  pageSize = 6;
+  customerDetail :any[] = [];
+  sparePartDetailList: any[] =[];
+  constructor(private activeRoute:ActivatedRoute,private _apiService:ApiService) { }
 
   ngOnInit(): void {
     this.activeRoute.params.subscribe(res=>{
         if(res['id']){
           this.paramId = res['id'];
-        }
-      this.getSparePartList()
+          this.getCustomerDetail(this.paramId);
+          this.getSparePartsDetail(this.paramId);
+        }       
+    }
+    )
+  }
+  getCustomerDetail(id:any){
+    let param = "OpportunityId="+id
+    this._apiService.getSparePartsWorkOrder(param).subscribe(res=>{
+      if(res.isSuccess){
+        this.customerDetail = res.data;
+      }else{
+        this.customerDetail = [];
+      }
     })
   }
-  getSparePartList() {
-    debugger
-    this.sparePartList = [
-      {
-        id: 1,
-        partNo: 'P001',
-        description: 'Oil filter',
-        qty: 3,
-        unitPrice: '20JD',
-        discount: '10%',
-        net: 0.000,
-        tax: '16%',
-        totalPrice: '60JD'
-      },
-      {
-        id: 2,
-        partNo: 'P001',
-        description: 'Oil filter',
-        qty: 3,
-        unitPrice: '20JD',
-        discount: '10%',
-        net: 0.000,
-        tax: '16%',
-        totalPrice: '60JD'
-      },
-      {
-        id: 3,
-        partNo: 'P001',
-        description: 'Oil filter',
-        qty: 3,
-        unitPrice: '20JD',
-        discount: '10%',
-        net: 0.000,
-        tax: '16%',
-        totalPrice: '60JD'
+  getSparePartsDetail(id:any){
+    this._apiService.getSparePartsWorkParts(id).subscribe(res=>{
+      if(res.isSuccess){
+        this.sparePartDetailList = res.data;
+      }else{
+        this.sparePartDetailList = [];
       }
-    ]
+    })
   }
+  // getSparePartList() {
+  //   debugger
+  //   this.sparePartList = [
+  //     {
+  //       id: 1,
+  //       partNo: 'P001',
+  //       description: 'Oil filter',
+  //       qty: 3,
+  //       unitPrice: '20JD',
+  //       discount: '10%',
+  //       net: 0.000,
+  //       tax: '16%',
+  //       totalPrice: '60JD'
+  //     },
+  //     {
+  //       id: 2,
+  //       partNo: 'P001',
+  //       description: 'Oil filter',
+  //       qty: 3,
+  //       unitPrice: '20JD',
+  //       discount: '10%',
+  //       net: 0.000,
+  //       tax: '16%',
+  //       totalPrice: '60JD'
+  //     },
+  //     {
+  //       id: 3,
+  //       partNo: 'P001',
+  //       description: 'Oil filter',
+  //       qty: 3,
+  //       unitPrice: '20JD',
+  //       discount: '10%',
+  //       net: 0.000,
+  //       tax: '16%',
+  //       totalPrice: '60JD'
+  //     }
+  //   ]
+  // }
 }
