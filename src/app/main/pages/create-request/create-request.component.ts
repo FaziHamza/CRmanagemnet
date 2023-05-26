7798ea2b37b030cec6ce28363be97e665a78e566
Amantatla <amantatla312@gmail.com>
2023-05-26 18:57:42 +0500

    modal

--- src/app/main/pages/create-request/create-request.component.ts
+++ src/app/main/pages/create-request/create-request.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NzModalService } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-create-request',
@@ -8,21 +9,24 @@ import { Component, OnInit } from '@angular/core';
 export class CreateRequestComponent implements OnInit {
   statusList: any[] = [];
   statusType = 0;
-  constructor() { }
+  constructor(private modal: NzModalService,) { }
 
   ngOnInit(): void {
     this.makeStatusList();
   }
-  makeStatusList(){
+  makeStatusList() {
     this.statusList = [
       {
         name: 'Reschedule PNs',
-        id:1,
+        id: 1,
       },
       {
         name: 'Transfer PNs',
-        id:2,
+        id: 2,
       }
     ]
+  }
+  cancelModal() {
+    this.modal.closeAll();
   }
 }
