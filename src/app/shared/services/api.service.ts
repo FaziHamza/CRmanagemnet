e128f9742c24694fc040d8666d1e1a143afc98a5
faizan <faizan1712818@gmail.com>
2023-05-28 21:04:31 +0500

    remove file

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -28,36 +28,7 @@ export class ApiService {
     let url = environment.creditmanagement + "Setup/GetCMSetup";
     return this.http.get<AppResponse>(url);
   }
-  getParts(partno:any):Observable<AppResponse> {
-    partno = partno + "&BranchId=" +this.getUser().branch.branchId
-    let url = environment.intermediate + "Parts/GetParts?part="+partno;
-    return this.http.get<AppResponse>(url);
-  }
-  getCustomer(name:string):Observable<AppResponse> {
-    let obj = "&PageNo=0&PageSize=10000"
-    let url = environment.intermediate + "Customer/GetCustomers?Search=" + name + obj;
-    return this.http.get<AppResponse>(url);
-  }
-  createWorkOrder(obj:any) :Observable<AppResponse> {
-    let url = environment.intermediate + "SalesOrders/CreateWorkOrder";
-    return this.http.post<AppResponse>(url,obj);
-  }
-  updateWorkOrder(obj:any) :Observable<AppResponse> {
-    let url = environment.intermediate + "SalesOrders/UpdateWorkOrder";
-    return this.http.post<AppResponse>(url,obj);
-  }
+  
 
-  getStatusLookup(id:number):Observable<AppResponse> {
-    let url = environment.intermediate + "Lookups/GetLookups?lookupTypeId="+id;
-    return this.http.get<AppResponse>(url);
-  }
-
-  getSparePartsWorkOrder(param:string=null):Observable<AppResponse> {
-    let url = environment.intermediate + "SalesOrders/GetSparePartsWorkOrders?"+param;
-    return this.http.get<AppResponse>(url);
-  }
-  getSparePartsWorkParts(param:string=null):Observable<AppResponse> {
-    let url = environment.intermediate + "SalesOrders/GetSparePartsWorkParts?sparePartsSalesOrderOpportunityId="+param;
-    return this.http.get<AppResponse>(url);
-  }
+  
 }
