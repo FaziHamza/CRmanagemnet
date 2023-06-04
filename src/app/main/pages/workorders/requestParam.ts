79d62c2e3f4b2f6e6be370608d60e2265225c7ae
faizan <faizan1712818@gmail.com>
2023-06-04 14:47:13 +0500

    need to add icons to the request

---
+++ src/app/main/pages/workorders/requestParam.ts
@@ -0,0 +1,11 @@
+export interface requestParam {
+    BranchId: number
+    Sort: number,
+    Status: number,
+    FromDate: any,
+    ToDate: any,
+    OrderNumber:string
+    PageSize:number
+    Search:string
+  }
+  
