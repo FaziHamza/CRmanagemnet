22681d1e0d4aa9befaaa2d7cc8089af3d24cb6d4
faizan <faizan1712818@gmail.com>
2023-06-11 11:21:52 +0500

    Fixed all issues

--- src/app/main/pages/promissory-note/promissory-note.component.ts
+++ src/app/main/pages/promissory-note/promissory-note.component.ts
@@ -180,8 +180,7 @@ export class PromissoryNoteComponent implements OnInit, AfterViewInit {
     const index = this.promissoryist.findIndex(item => item.id === id);
     Object.assign(this.editCache[id].data, this.promissoryist[index]);
     this.editCache[id].edit = false;
-
-   
+    
     let amount = 0;
     for (let index = 0; index < this.promissoryist.length; index++) {
       amount += this.editCache[index + 1].data.amount ?  parseFloat(this.editCache[index + 1].data.amount) : 0;
@@ -341,11 +340,11 @@ export class PromissoryNoteComponent implements OnInit, AfterViewInit {
       // charCode 48-57 are 0-9, 46 is .
       return false;
     }
-    // let amount = 0;
-    // for (let index = 0; index < this.promissoryist.length; index++) {
-    //   amount += this.editCache[index + 1].data.amount ?  parseFloat(this.editCache[index + 1].data.amount) : 0;
-    // }
-    // this.differenceAmount = parseFloat((this.orderDetail.pnTotalAmount - parseFloat(amount.toFixed(3))).toFixed(3));
+    let amount = 0;
+    for (let index = 0; index < this.promissoryist.length; index++) {
+      amount += this.editCache[index + 1].data.amount ?  parseFloat(this.editCache[index + 1].data.amount) : 0;
+    }
+    this.differenceAmount = parseFloat((this.orderDetail.pnTotalAmount - parseFloat(amount.toFixed(3))).toFixed(3));
   }
   numericOnly(event): boolean {
     const charCode = (event.which) ? event.which : event.keyCode;
